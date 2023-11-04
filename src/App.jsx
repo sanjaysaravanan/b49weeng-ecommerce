b206b07d633de812c6548eda7e42525084b87125
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-11-04 12:54:26 +0530

    added outlet routing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
 
-import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import PropTypes from "prop-types";
 import Ecom from "./Ecom";
 import Cart from "./Cart";
@@ -26,12 +33,45 @@ PageNotFound.propTypes = {
   autoRedirect: PropTypes.bool.isRequired,
 };
 
+function Layout() {
+  return (
+    <div>
+      <header
+        style={{
+          backgroundColor: "blueviolet",
+          width: "100vw",
+          position: "fixed",
+          top: 0,
+          minHeight: "75px",
+        }}
+      ></header>
+      <aside
+        style={{
+          width: 250,
+          position: "fixed",
+          top: 75,
+          backgroundColor: "#d1ffc4",
+          height: "calc(100vh - 75px)",
+        }}
+      ></aside>
+      <div
+        style={{
+          marginLeft: "260px",
+          marginTop: "80px",
+        }}
+      >
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Ecom />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Ecom />} />
           <Route path="/products">
             <Route index element={<Ecom />} />
             {/* Url for belopw route /products/<product-id> */}
@@ -42,9 +82,9 @@ function App() {
           <Route path="/:productId" element={<ProductPage />} />
           <Route path="/profile" element={<ProfileComponent />} />
           <Route path="*" element={<PageNotFound autoRedirect={false} />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
